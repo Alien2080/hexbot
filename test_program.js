c5fd0f22e05976c07090dd07c10bf6f5ecdaa4cb
Alien2080 <tom_aylen@hotmail.com>
2019-06-27 21:27:54 +0930

    made it flash colours

--- test_program.js
+++ test_program.js
@@ -2,10 +2,16 @@ let helloWorld;
 
 function start_app() {
 
-    NOOPBOT_FETCH({API: 'hexbot'}, changeColor); 
+    let speed = 500;
+    NOOPBOT_TICK_SETUP(getColour, speed);
 
 }
 
-function changeColor(responseJson){  
+function getColour() {
+    NOOPBOT_FETCH({ API: 'hexbot' }, changeColor);
+}
+
+function changeColor(responseJson) {
+    console.log(responseJson);
     document.getElementById("helloWorld").style.color = responseJson.colors[0].value;
 }
